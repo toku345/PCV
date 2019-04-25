9cfaf5093a59f073234362edc9f2cc3f6bde88c7
Fumitaka Tokumitsu <toku345@gmail.com>
2019-04-25 09:49:21 +0900

    1.3.3 Add imresize()

--- shakyo/imtools.py
+++ shakyo/imtools.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
+from PIL import Image
+
 
 def get_imlist(path):
     """ pathに指定されたディレクトリのすべてのjpgファイル名のリストを返す """
     return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg')]
+
+
+def imresize(im, sz):
+    """ PILをつかって画像サイズを変更する """
+    pil_im = Image.fromarray(uint8(im))
+    return array(pil_im.resize(sz))
